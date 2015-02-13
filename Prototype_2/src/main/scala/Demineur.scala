d47c3dc89d8915c8dedb044fd0c3b07d63779ee1
Wyrdmaker <thomasdupriez@laposte.net>
2015-02-13 07:57:42 +0100

    ajout dossier scala dans prototype 2

---
+++ Prototype_2/src/main/scala/Demineur.scala
@@ -0,0 +1,17 @@
+import scala.swing._
+import scala.swing.event._
+import javax.swing.{ImageIcon, Icon}
+
+
+
+class UI extends MainFrame {
+	
+}
+
+
+object GuiProgram {
+	def main(args: Array[String]) {
+		val ui = new UI
+		ui.visible = true
+	}
+}
---
+++ Prototype_2/src/main/scala/GUI.scala
@@ -0,0 +1,4 @@
+import scala.swing._
+import scala.swing.event._
+import javax.swing.{ImageIcon, Icon}
+
